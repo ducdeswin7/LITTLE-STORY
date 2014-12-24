01285b7b29a1d2e4319c504b1a09b8226b58fd1d
ducdeswin7 <ducdeswin@outlook.com>
2014-12-24 22:18:56 +0100

    update

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+<head>
+
+<meta charset="utf-8">
+
+<meta name="description" content="A responsive layout with fancy scrolling effec" />
+<meta name="keywords" content="responsive layout, scrolling effect" />
+<meta name="author" content="PeHaa for Pepsizez" />
+<title>My story</title>
+<link href='http://fonts.googleapis.com/css?family=Ovo' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="stylesheets/screen.css" type="text/css" media="screen" title="no title" charset="utf-8">
+</head>
+ 
+<body>
+<header class=" content">
+	<div class="wrapper">
+	<h1>Du passé au futur.</h1>
+        <p>Si je me rappelle de la version que mes parents me racontaient, j'étais ce gamin qui détruisait tous ses jouets afin de découvrir ce qu'il y avait à l'intérieur, plus précisément mes jouets électroniques. Mais je reconnais, je me souviens de quelques jouets électroniques que j'avais détruit. :-) .</p>
+ 
+    <p>En grandissant, j'avais comme ambition de ressembler à l'un de mes parents. Étant donné que je suis un garçon, vous pensez que j’aurai souhaité ressemblé à mon père, (rire) Désolé de vous décevoir mais vous avez tort. Je voulais ressembler à ma mère qui était pour moi l'exemple d'une personne indépendante, battante, déterminée, prête à nous donner le meilleur mais surtout elle possédait un caractère de fer.
+</p>
+	
+	</div>
+</header>
+
+<section class="content" id="oil">
+	<div class="wrapper">
+	<p>Commerçante et entrepreneuse, j'ai reçu d'elle des qualités dont je ne saurai vous en parler. Malgré cela je n'ai jamais perdu mon envie inconditionnel de découvrir et de comprendre la technologie au contraire elle grandissait, je me rappelle que déjà au CM1 j'avais déjà un adresse email afin de pouvoir me connecter sur des sites de jeux video, ce qui m'a fait découvrir un nouveau outil technologique appelé Internet.</p>
+    <p>Il m’a permis d'apprendre beaucoup de choses sur la technologie. Mais voulant toujours suivre la voie de ma mère je me suis lancé dans des études de gestion après mon BEPC, ce qui ne m’a pas empêché de continuer à apprendre à découvrir plus sur l'internet. 
+</p>
+	</div>
+
+</section>
+<section class="content " id="culinary">
+	<div class="wrapper">
+	<p>Voulant être autonome comme ma mère, j'ai donc décidé de poursuivre mes études dans le commerce afin de pouvoir créer mon entreprise. C’est ainsi que je me suis inscrit dans une école supérieur de gestion et de commerce a Paris afin d'acquérir les capacités requises pour être un bon manager.</p>
+	</div>
+</section>
+
+<section class="content " id="dried">
+	<div class="wrapper">
+	<h2>Direct vers le but</h2>
+	<p>Après une année de formation en gestion tout en continuant à apprendre la technologie, j'ai finalement choisi ma route et je sais ce que j’aimerai devenir plus tard supposons dans 10 ans.</p>
+
+    <p>Dans 10 ans, je créerai mon entreprise de développement informatique des sites web, des applications informatiques avec une spécialisation dans la sécurité informatique dans plusieurs pays apres avoir acquis le maximum de connaissances au cours des stages et . C’est dans cette vision que j'ai renoncé à l'école de commerce pour rejoindre la première école du Web en France, connue sous le nom HETIC.
+</p>
+	</div>
+</section>
+<footer>
+ 	<div class=wrapper>
+		<p>Made by <strong><a href="mailto:boevilawson@outlook.com">Boevi LAWSON</a></strong></p>
+		
+	</div>
+</footer>
+</body>
+</html>
